0c5d803c8124a89d7052d0903f540d917b264657
Jun Aruga <jaruga@redhat.com>
2023-07-25 18:59:12 +0200

    Add reproducer.c based on the 20657.c.
    To debug the issue easier.

--- README.md
+++ README.md
@@ -2,17 +2,79 @@
 
 This repository is to manage the files related to [this issue](https://github.com/openssl/openssl/issues/20657) on OpenSSL project.
 
-## A test program
-
 The reproducing program was downloaded from the <https://gist.github.com/levitte/7a27cebdb9537ff0a59641c9a5bed53d>.
 
-### Reproducing steps
+## Reproducing steps
 
 Create `key.pem` if you want to create newly.
 
 ```
 $ openssl genrsa -out key.pem 4096
 ```
+
+Compile
+
+```
+$ gcc \
+  -I /home/jaruga/.local/openssl-3.2.0.dev-fips-debug-06a0d40322/include \
+  -L /home/jaruga/.local/openssl-3.2.0.dev-fips-debug-06a0d40322/lib \
+  -o reproducer reproducer.c -lcrypto
+```
+
+### Non-FIPS case
+
+This is ok.
+
+```
+$ OPENSSL_CONF_INCLUDE=/home/jaruga/.local/openssl-3.2.0.dev-fips-debug-06a0d40322/ssl \
+  OPENSSL_MODULES=/home/jaruga/.local/openssl-3.2.0.dev-fips-debug-06a0d40322/lib/ossl-modules \
+  LD_LIBRARY_PATH=/home/jaruga/.local/openssl-3.2.0.dev-fips-debug-06a0d40322/lib \
+  ./reproducer key.pem
+[DEBUG] Loaded providers:
+  default
+[DEBUG] FIPS mode enabled: 0
+[DEBUG] data_size: 3272
+[DEBUG] OSSL_DECODER_from_bio PEM failed.
+[DEBUG] Got a pkey! 0x10ddcf0
+[DEBUG] It's held by the provider default
+[DEBUG] ossl_membio2str buf->data:
+-----BEGIN PUBLIC KEY-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA1zjzeiqsSJW0QK7MCi6+
+UYWu5uLHrPucubpS1Y2hhfUDQAkeJeXwb/iv6arSptXRbQK5GcVHvn21t5KuraQe
+INhnBc1+O95WRPKnFjR3yUAXpoYJ8QuuIUTKAO+bh10qVapZyhDDj5/Gr3YsKgUz
+Vyr4LHckAYmLkJhQjcC1jqrXUykLbrxJh/vSJtW+3yOOYQnOb1BFUsdcA0SHjfOC
+Gn6n/D02Jgy6czhdnI7vVRpQ0YsY5AmDW7PI3JlCOFCNy2c1l3+BgyU7c8PFscZJ
+gRijZVWMujhEOaW15PEt8Dg3MG2gcb5zxNR57rj+sPbFAQtcZarDp0/1nxymEwtO
+1bAwZfVuz9s2a52denBV39zo+YAZ3dKWXxF0Dn2tJQfzpcmIJmuY4/sp5tCkVkoe
+okchoDf7BZjX+p60JwvWUmLvAk5zgbLS60jpHjT4hy6qlVnGGkItQGv8nmaU4Iw6
+zAB6Pr+YabF7qAz0NnjDQn42DDLKfCthQ93B8KKo2vfTMQpiMd2DaJBXORaBIv1W
+C1vbZwj4BIroZzYCMbENOBotXB2DR17YrTRe9TgAToHlGfEpnBFmO6K0OQcAnAAh
+KxeWZ8vQ5KiY/2w5wjpKQpNZFZYw3dTtcmOrRP7pmaVObkZ6FaD4WHOn6lKscn2L
+lZ/HcBXdBPtGJ/W/LhsqZs0CAwEAAQ==
+-----END PUBLIC KEY-----
+```
+
+### FIPS case
+
+This is not ok.
+
+```
+$ OPENSSL_CONF=$(pwd)/openssl_fips.cnf \
+  OPENSSL_CONF_INCLUDE=/home/jaruga/.local/openssl-3.2.0.dev-fips-debug-06a0d40322/ssl \
+  OPENSSL_MODULES=/home/jaruga/.local/openssl-3.2.0.dev-fips-debug-06a0d40322/lib/ossl-modules \
+  LD_LIBRARY_PATH=/home/jaruga/.local/openssl-3.2.0.dev-fips-debug-06a0d40322/lib \
+  ./reproducer key.pem
+[DEBUG] Loaded providers:
+  fips
+  base
+[DEBUG] FIPS mode enabled: 1
+[DEBUG] data_size: 3272
+4067B53FF37F0000:error:1E08010C:DECODER routines:OSSL_DECODER_from_bio:unsupported:crypto/encode_decode/decoder_lib.c:102:No supported data to decode. Input type: DER
+[DEBUG] Failed to get the pkey.
+[DEBUG] Could not parse a PKey
+```
+
+## 20657.c (original reproducing program)
 
 Run the commands below.
 
---
+++ reproducer.c
@@ -0,0 +1,166 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include <openssl/decoder.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/bio.h>
+#include <openssl/provider.h>
+
+static int print_provider(OSSL_PROVIDER *prov, void *unused)
+{
+    printf("  %s\n", OSSL_PROVIDER_get0_name(prov));
+    return 1;
+}
+
+EVP_PKEY *
+ossl_pkey_read_generic(BIO *bio, char *pass)
+{
+    OSSL_DECODER_CTX *dctx;
+    EVP_PKEY *pkey = NULL;
+    int pos = 0, pos2;
+
+    dctx = OSSL_DECODER_CTX_new_for_pkey(&pkey, "DER", NULL, NULL, 0, NULL, NULL);
+    if (!dctx) {
+        fprintf(stderr, "[DEBUG] OSSL_DECODER_CTX_new_for_pkey DER failed.\n");
+        goto out;
+    }
+    if (OSSL_DECODER_CTX_set_pem_password_cb(dctx, PEM_def_callback, pass) != 1) {
+        fprintf(stderr, "[DEBUG] OSSL_DECODER_CTX_set_pem_password_cb DER failed.\n");
+        goto out;
+    }
+
+    /* First check DER */
+    if (OSSL_DECODER_from_bio(dctx, bio) == 1) {
+        fprintf(stderr, "[DEBUG] OSSL_DECODER_from_bio DER failed.\n");
+        goto out;
+    }
+    BIO_reset(bio);
+
+    /* Then check PEM; multiple OSSL_DECODER_from_bio() calls may be needed */
+    if (OSSL_DECODER_CTX_set_input_type(dctx, "PEM") != 1) {
+        fprintf(stderr, "[DEBUG] OSSL_DECODER_CTX_set_input_type PEM failed.\n");
+        goto out;
+    }
+
+    OSSL_DECODER_CTX_set_selection(dctx, EVP_PKEY_KEYPAIR);
+    while (1) {
+        if (OSSL_DECODER_from_bio(dctx, bio) == 1) {
+            fprintf(stderr, "[DEBUG] OSSL_DECODER_from_bio PEM failed.\n");
+            goto out;
+        }
+        ERR_print_errors_fp(stderr);
+        if (BIO_eof(bio))
+            break;
+        pos2 = BIO_tell(bio);
+        if (pos2 < 0 || pos2 <= pos)
+            break;
+        ERR_clear_error();       /* Maybe print? */
+        pos = pos2;
+    }
+
+    BIO_reset(bio);
+    OSSL_DECODER_CTX_set_selection(dctx, 0);
+    while (1) {
+        if (OSSL_DECODER_from_bio(dctx, bio) == 1) {
+            fprintf(stderr, "[DEBUG] OSSL_DECODER_CTX_new_for_pkey PEM 2 failed.\n");
+            goto out;
+        }
+        ERR_print_errors_fp(stderr);
+        if (BIO_eof(bio))
+            break;
+        pos2 = BIO_tell(bio);
+        if (pos2 < 0 || pos2 <= pos)
+            break;
+        ERR_clear_error();       /* Maybe print? */
+        pos = pos2;
+    }
+
+    if (!pkey) {
+        fprintf(stderr, "[DEBUG] Failed to get the pkey.\n");
+    }
+out:
+    OSSL_DECODER_CTX_free(dctx);
+    return pkey;
+}
+
+int main(int argc, char *argv[])
+{
+    int status = EXIT_SUCCESS;
+    int fips_enabled = 0;
+    /* Input file */
+    FILE *f = NULL;
+    static char data[1024 * 1024];
+    size_t data_size = 0;
+    EVP_PKEY *pkey = NULL;
+    BIO *bio = NULL;
+    BUF_MEM *buf = NULL;
+
+    /* Print debugging info for FIPS */
+    printf("[DEBUG] Loaded providers:\n");
+    OSSL_PROVIDER_do_all(NULL, &print_provider, NULL);
+    fips_enabled = EVP_default_properties_is_fips_enabled(NULL);
+    printf("[DEBUG] FIPS mode enabled: %d\n", fips_enabled);
+
+    /* Check if the input file from the first argument is valid. */
+    if ((f = fopen(argv[1], "r")) == NULL
+        || (data_size = fread(data, 1, sizeof(data), f)) == 0) {
+        fprintf(stderr, "[DEBUG] Could not read PKey: %s\n", argv[1]);
+        status = EXIT_FAILURE;
+        goto end;
+    }
+    printf("[DEBUG] data_size: %d\n", data_size);
+
+    fclose(f);
+    f = NULL;
+
+    data[data_size] = '\0';
+
+    /* Get Pkey from data. */
+    bio = BIO_new_mem_buf(data, strlen(data));
+    if (!bio) {
+        fprintf(stderr, "[DEBUG] BIO_new_mem_buf() failed\n");
+        status = EXIT_FAILURE;
+        goto end;
+    }
+    pkey = ossl_pkey_read_generic(bio, "");
+    BIO_free(bio);
+    bio = NULL;
+    if (!pkey) {
+        fprintf(stderr, "[DEBUG] Could not parse a PKey\n");
+        status = EXIT_FAILURE;
+        goto end;
+    }
+
+    printf("[DEBUG] Got a pkey! %p\n", (void *)pkey);
+    printf("[DEBUG] It's held by the provider %s\n",
+           OSSL_PROVIDER_get0_name(EVP_PKEY_get0_provider(pkey)));
+
+    /* ossl_pkey_export_spki(pkey, 0); */
+    /* Export Pkey. */
+    bio = BIO_new(BIO_s_mem());
+    if (!bio) {
+        fprintf(stderr, "BIO_new\n");
+        status = EXIT_FAILURE;
+        goto end;
+    }
+    if (!PEM_write_bio_PUBKEY(bio, pkey)) {
+        fprintf(stderr, "PEM_write_bio_PUBKEY\n");
+        status = EXIT_FAILURE;
+        goto end;
+    }
+    BIO_get_mem_ptr(bio, &buf);
+    printf("[DEBUG] ossl_membio2str buf->data:\n%s\n", buf->data);
+end:
+    if (f) {
+        fclose(f);
+    }
+    if (bio) {
+        BIO_free(bio);
+    }
+    if (pkey) {
+        EVP_PKEY_free(pkey);
+    }
+    return status;
+}
